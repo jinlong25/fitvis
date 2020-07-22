ed24846059f8edd697d7c28b26af2588918c8271
Jinlong Yang <jinlong25@users.noreply.github.com>
2020-07-21 21:58:22 -0700

    replace step radial bar chart with step raial area chart

--- js/script.js
+++ js/script.js
@@ -1,15 +1,10 @@
-// import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
-// import * as d3 from 'd3';
-// import * as moment from 'moment';
-// import './style.css';
-
 //create a namespace for common vars
 var fv = {
 	'top': 10,
 	'right': 10,
 	'bottom': 10,
 	'left': 10,
-	'outerWidth': 300
+	'outerWidth': 200
 };
 
 fv.outerHeight = fv.outerWidth;
@@ -20,10 +15,9 @@ fv.height = fv.outerHeight - fv.top - fv.bottom;
 //create a namespace for step
 var step = {
 	'outerRadius': fv.outerWidth/2,
-	'barHeight': 75,
-	'barWidth': 3
+	'areaHeight': 30
 };
-step.innerRadius = fv.outerWidth/2 - step.barHeight;
+step.innerRadius = fv.outerWidth/2 - step.areaHeight;
 
 //create a namespace for sleep
 var sleep = {
@@ -35,7 +29,6 @@ sleep.radius = step.innerRadius - sleep.arcWidth,
 d3.json('/data/data.json').then(function(data) {
 
 	//subset dates from data
-	//fv.dates = Object.getOwnPropertyNames(data).slice(0, 70);
 	fv.dates = Object.getOwnPropertyNames(data);
 
 	fv.dates.forEach(function(d, i){
@@ -75,7 +68,7 @@ d3.json('/data/data.json').then(function(data) {
 			.attr('height', fv.outerHeight)
 			.attr('x', -fv.outerWidth/2)
 			.attr('y', -fv.outerHeight/2)
-			.style('fill', '#fff');
+			.style('fill', '#ddd');
 
 		//create a todayExtent
 		chart.todayExtent = todayExtent(d);
@@ -114,43 +107,30 @@ function drawStep(data, date, chart) {
 		d['dt-la'] = moment(d['dt-la'], 'YYYY-MM-DD HH:mm:SS');
 	});
 
-	// create scales
-	step.barScale = d3.scaleLinear()
+	//define x scale
+	step.x = d3.scaleTime()
+		.domain(d3.extent(step.data.map(d => d['dt-la'])))
+		.range([0, 2 * Math.PI]);
+
+	//define y scale
+	step.y = d3.scaleLinear()
+		.domain([0, 2000])//##hard-code max value for now
 		.range([step.innerRadius, step.outerRadius])
-		.domain([0, 1500]);
 
-	step.angleScale = d3.scaleLinear()
-		.range([-180, 180])
-		.domain(todayExtent(date));
+	//define area function
+	step.area = d3.areaRadial()
+		// .curve(d3.curveLinearClosed)
+		.curve(d3.curveCardinalClosed)
+		.angle(d => step.x(d['dt-la']));
 
-	step.colorScale = d3.scaleLinear()
-		.domain([0, 0, 1000])
-		.range(['#2c7bb6', '#ffff8c', '#d7191c'])
-		.interpolate(d3.interpolateHcl);
-
-	// create a bar wrapper
-	step.barWrapper = chart.svg.append('g')
-	.attr('transform', 'translate(' + 0 + ',' + 0 + ')');
-
-	//draw bars
-	step.barWrapper.selectAll('.step-bar')
-		.data(step.data)
-		.enter().append('rect')
-		.attr('class', 'step-bar')
-		.attr('transform', function(d, i) { return 'rotate(' + (step.angleScale(d['dt-la'])) + ')'; })
-		.attr('width', step.barWidth)
-		.attr('height', function(d, i) { return step.barScale(d.value) - step.innerRadius; })
-		.attr('x', -1)
-		.attr('y', step.innerRadius)
-		.style('fill', function(d) { return step.colorScale(d.value); })
-		.attr('data-dt', function(d) { return d['dt-la']; })
-		.attr('data-steps', function(d) { return d.value; });
-
-	//add mouseover info printing
-	d3.selectAll('.step-bar').on('mouseover', function() {
-		console.log(d3.select(this).attr('data-dt'));
-		console.log(d3.select(this).attr('data-steps'));
-	});
+	//draw step radial area chart
+	chart.svg.append('path')
+		.attr('fill', '#d7191c')
+		.attr('d', step.area
+			.innerRadius(step.y(0))
+			.outerRadius(d => step.y(d.value))
+			(step.data)
+		);
 }
 //-----------------------------------------------------step END
 
